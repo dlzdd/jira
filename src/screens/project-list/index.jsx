acf73f83e0278a249195d36bfa0d1a763d62d460
dlzdd <1102474946@qq.com>
2022-11-22 21:40:19 +0800

    初步实现登陆页面及接口实现

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,5 +1,7 @@
 import { SearchPanel } from "./search-panel"
 
+const apiUrl = process.env.REACT_APP_API_URL
+
 export const ProjectListScreen = () => {
     return <div>
         <SearchPanel></SearchPanel>
